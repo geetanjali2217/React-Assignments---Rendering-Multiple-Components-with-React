e8f8636e3c47c31f1b7341c9d901a6370ece5b63
Geetanjali Katare <68340329+geetanjali2217@users.noreply.github.com>
2020-11-22 23:10:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <>
+     <div data-ns-test="project-name">Note Sharing Application</div>
+<div data-ns-test="project-description">
+In this application you can share the notes and also can dowload others notes.
+</div>
+    </>
   )
 }
 
